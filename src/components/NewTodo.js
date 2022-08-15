12c890d3d0cf45bf47cc68c25bed1c1c51592f78
Ariful Islam Polash <arifulislampolash.25.fswad@gmail.com>
2022-08-15 18:34:05 +0600

    functionality added newTodo

--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React , {useState} from 'react'
 import style from './NewTodo.module.css'
+
 function NewTodo() {
+
+  const {item, setItem}=useState({title:" ", desc:" "});
+  const {title, desc}=item;
+
+
+  const handleChange=(event)=>{
+    const name= event.target.name;
+    setItem((oldItem) => {
+      return {...oldItem , [name]:event.target.value};
+    });
+  }
+
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    console.log(item);
+    setItem({ title:" ", desc: " "});
+  }
+
+
   return (
-    <form className={style.form}>
+    <form className={style.form} onSubmit={handleSubmit}>
         <div className={style["form-field"]}>
             <label htmlFor="title">Title:</label>
-            <input type="text" name="title" id="title"></input>
+            <input type="text" name="title" id="title" value={title} placeholder="title"></input>
         </div>
         <div className={style["form-field"]}>
             <label htmlFor="desc">Description:</label>
-            <input type="text" name="desc" id="desc"></input>
+            <input type="text" name="desc" id="desc" value={desc} placeholder="Description"></input>
         </div>
         <button type="submit"> Add Todo</button>
     </form>
